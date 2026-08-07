42bdd01121f92f51247421e012a75039aab6f841
mbasm5345-boop <mbasm5345@gmail.com>
2026-08-07 19:44:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(__dirname));
+
 app.post("/chat", (req, res) => {
 
     const message = req.body.message;
