90e4272238902d80c36d559ef90a69e2ed6cfb4a
mbasm5345-boop <mbasm5345@gmail.com>
2026-08-07 19:35:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const sendBtn = document.getElementById("sendBtn");
+const userInput = document.getElementById("userInput");
+const chatBox = document.getElementById("chatBox");
+
+sendBtn.addEventListener("click", async function () {
+
+    let message = userInput.value;
+
+    if (message.trim() === "") return;
+
+    chatBox.innerHTML += `
+    <p>👤 أنت: ${message}</p>
+    `;
+
+    userInput.value = "";
+
+    let response = await fetch("http://localhost:3000/chat", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            message: message
+        })
+    });
+
+    let data = await response.json();
+
+    chatBox.innerHTML += `
+    <p>🤖 Nexus AI: ${data.reply}</p>
+    `;
+
+});
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/chat", (req, res) => {
+
+    const message = req.body.message;
+
+    res.json({
+        reply: "🤖 استلمت طلبك: " + message
+    });
+
+});
+
+app.listen(3000, () => {
+    console.log("Nexus AI running");
+});
